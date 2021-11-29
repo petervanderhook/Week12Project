c7971e3da87c44ffa2a9a339b5e2780a7d2941fa
petervanderhook <petervanderhook@gmail.com>
2021-11-28 23:13:29 -0800

    Added save new contact function

--- script.js
+++ script.js
@@ -55,46 +55,57 @@ function renderIndex(contact_array) {
     contact.insertAdjacentHTML('beforeend', html)
 }
 
-function renderCreate(  ) {
+function renderCreate() {
     let contactedit = document.querySelector('.main')
-    contactedit.insertAdjacentHTML('beforeend', `<div class="contactedit">
-    <div class="contactimg">
-        <img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
-    </div>
-    <div class="form">
-        <form>
-            <div class="inputcontainer">
-                <input type="text" id="contactname" name="contactname" placeholder="Contact Name">
-                <button class="extrafield" id="extranamefield" name="extranamefield">+</button>
-            </div>
+    contactedit.insertAdjacentHTML('beforeend', 
+    `<div class="contactedit">
+        <div class="contactimg">
+            <img src="./img/profile.jpg" class ="profilepic" alt="Profile picture">
+        </div>
+        <div class="form">
+            <form>
+                <div class="inputcontainer">
+                    <input type="text" id="contactname" name="contactname" placeholder="Contact Name">
+                    <button class="extrafield" id="extranamefield" name="extranamefield">+</button>
+                </div>
 
-            <div class="inputcontainer">
-                <input type="tel" id="contactphone" name="contactphone" placeholder="Contact Phone">
-                <button class="extrafield" id="extraphonefield" name="extraphonefield">+</button>
-            </div>
+                <div class="inputcontainer">
+                    <input type="tel" id="contactphone" name="contactphone" placeholder="Contact Phone">
+                    <button class="extrafield" id="extraphonefield" name="extraphonefield">+</button>
+                </div>
 
-            <div class="inputcontainer">
-                <input type="text" id="contactaddress" name="contactaddress" placeholder="Contact Address">
-                <button class="extrafield" id="extraaddressfield" name="extraaddressfield">+</button>
-            </div>
+                <div class="inputcontainer">
+                    <input type="text" id="contactaddress" name="contactaddress" placeholder="Contact   Address">
+                    <button class="extrafield" id="extraaddressfield" name="extraaddressfield">+</button>
+                </div>
             
-            <div class="inputcontainer">
-                <input type="email" id="contactemail" name="contactemail" placeholder="Contact Email">
-                <button class="extrafield" id="extraemailfield" name="extraemailfield">+</button>
-            </div>
+                <div class="inputcontainer">
+                    <input type="email" id="contactemail" name="contactemail" placeholder="Contact Email">
+                    <button class="extrafield" id="extraemailfield" name="extraemailfield">+</button>
+                </div>
 
-            <div class="buttons">
-                <button type="submit" class="button save" id="savecontact" name="savecontact">Save Contact</button>
-                <button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
-            </div>
-        </form>
-    </div>
-</div>`)
+                <div class="buttons">
+                    <button type="submit" class="button save" id="savecontact" name="savecontact">Save  Contact</button>
+                    <button type="reset" class="button cancel" id="cancel" name="cancel">Cancel</button>
+                </div>
+            </form>
+        </div>
+    </div>`)
+    let form = document.querySelector('.form')
+    form.addEventListener('click', function(evt) {
+        evt.preventDefault()
+        evt.stopImmediatePropagation()
+    })
+    createSaveButton()
+    createCancelButton()
 }
+
+
 
 function renderView(contactname) {
     for (let i = 0; i < contactList.length; i++) {
         if (contactList[i].name == contactname) {
+            console.log(`rendering ${contactList[i].name}`)
             let contactinfo = document.querySelector('.main'); 
             contactinfo.insertAdjacentHTML('beforeend', `<div class="contactinfo">
             <div class="contactname">
@@ -149,6 +160,31 @@ function contactButtonsEvent() {
             
         })
     }
+}
+function createSaveButton() {
+    var saveButton = document.querySelector('#savecontact.button.save')
+    saveButton.addEventListener('click', function(evt) {
+        evt.preventDefault()
+        evt.stopImmediatePropagation()
+        var newcontact = new Object()
+        newcontact.name = document.getElementById('contactname').value
+        newcontact.phone = document.getElementById('contactphone').value
+        newcontact.address = document.getElementById('contactaddress').value
+        newcontact.email = document.getElementById('contactemail').value
+        contactList.push(newcontact)
+        cleanUpIndex()
+        renderIndex(contactList)
+    })
+}
+
+function createCancelButton() {
+    var cancelButton = document.querySelector('#cancel.button.cancel')
+    cancelButton.addEventListener('click', function(evt) {
+        evt.preventDefault()
+        evt.stopImmediatePropagation()
+        cleanUpIndex()
+        renderIndex(contactList)
+    })
 }
 
 function viewButtonsEvent() {
